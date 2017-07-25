10201ad6e3f27505c47c8d6a8e7d4e2b7ef8888d
LiWenxiao <m15800937069@163.com>
2017-07-25 15:44:42 +0800

    ssm搭好

---
+++ ProcurementSystem/src/main/java/com/ProcurementSystem/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.ProcurementSystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorld {
+	@RequestMapping(value = "index")
+	public String toLoginPage(){
+		return "index";
+	}
+}
