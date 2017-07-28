816dda0de9ed50e4d839f6fdfdedda46625632b8
Savannah314 <shengzhixuan314@126.com>
2017-07-28 15:58:36 +0800

    用户详情界面连接到mysql

--- ProcurementSystem/src/main/java/com/ProcurementSystem/controller/HelloWorld.java
+++ ProcurementSystem/src/main/java/com/ProcurementSystem/controller/HelloWorld.java
@@ -2,12 +2,11 @@ package com.ProcurementSystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorld {
 	@RequestMapping(value = "index")
 	public String toLoginPage(){
-		return "downStream/commodityCatalogue/procurementCommodityCatalog";
+		return "page/index";
 	}
 }
